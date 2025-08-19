597e4e73ae6b16328d8667de2e631f5525821eb9
Sumsin5969 <55352372+Sumsin5969@users.noreply.github.com>
2025-08-19 12:25:16 +0900

    Add HEInit and HEExit functions to engine API
    Introduced HEInit and HEExit functions for explicit engine initialization and cleanup. Fixed a typo in glBindTextureUnit call in RenderManager.cpp. Updated binary outputs to reflect these changes.

--- Hog_Engine/Hog_Engine/Extern/Engine.h
+++ Hog_Engine/Hog_Engine/Extern/Engine.h
@@ -8,9 +8,12 @@
 #include <chrono>
 #include <thread>
 
+void HE
 void HERun(const char* title, int width, int height, const int targetFPS, int VSync, std::unique_ptr<GameState> TargetState);
+void HEInit();
 void HEUpdate();
 void HESetNextGameState(std::unique_ptr<GameState> TargetState);
+void HEExit(void);
 void HESetWindowTitle(const char* title);
 float GetFPS();
 float GetDT();
